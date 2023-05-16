6259cc327e06dfc5d137392c348816ab0363e18e
daiwei <csdaiwei@foxmail.com>
2023-05-16 18:39:23 +0800

    finish hw5

--- hw5/Triangle.hpp
+++ hw5/Triangle.hpp
@@ -11,6 +11,13 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    Vector3f E1=v1-v0, E2=v2-v0, S=orig-v0;
+    Vector3f S1 = crossProduct(dir, E2), S2=crossProduct(S, E1);
+    auto denom = dotProduct(S1, E1);
+    tnear = dotProduct(S2, E2) / denom;
+    u = dotProduct(S1, S) / denom;
+    v = dotProduct(S2, dir) / denom;
+    if(tnear>=0 && u>=0 && v>=0 && (1-u-v)>=0) return true;
     return false;
 }
 
